17c9e73e8f8e7c7405f55a70a703393fb51e8c9a
Hrishikesh Jadhav <91720399+JHrishi@users.noreply.github.com>
2022-01-08 16:37:50 +0530

    Add files via upload

---
+++ virtual_pen.py
@@ -0,0 +1,50 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+cap = cv.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+
+def draw(mypoints):
+    for point in mypoints:
+        cv.circle(result, (point[0], point[1]), 10, (0, 255, 0), cv.FILLED)
+
+
+newpoints = []
+
+while True:
+    success, img = cap.read()
+    img = cv.flip(img, 1)
+    result = img.copy()
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                if id == 8:
+                    cv.circle(result, (cx, cy), 10, (0,0,255),cv.FILLED)
+                    count = 0
+                    if cx != 0 and cy != 0:
+                        newpoints.append([cx, cy])
+                    count += 1
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    if len(newpoints) != 0:
+        draw(newpoints)
+    pTime = cTime
+    cv.imshow('Image', result)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
